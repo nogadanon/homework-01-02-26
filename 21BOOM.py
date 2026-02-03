256185131c6ab6a527126601f533a1face7c97a4
Noga <nogakol1@gmail.com>
2026-02-03 13:36:32 +0200

    משחק 21BOOM מושלם

---
+++ 21BOOM.py
@@ -0,0 +1,88 @@
+#START
+
+import random
+ans = True
+ans_1 = True
+i = 1  # Number of cards in the same turn
+x = 1  # Determines which player's turn it is
+_sum1 = 0
+_sum2 = 0
+game_over = 0
+
+while True:
+
+    # Player choos if want another card
+    while i > 2 or x > 2 and game_over == 0:
+        player_input:int = input('Want another card? Answer yes / no only').lower()
+        ans = player_input == 'yes'
+
+        # change turn
+        if ans == False:
+            if x % 2 != 0: # player 1
+                print("Player's 2 turn:")
+            else: print("Player's 1 turn:")  # Player 2
+            x += 1
+            i = 1
+
+            # choice:
+            if x > 2:
+                player_input: int = input('Want another card? Answer yes/no only').lower()
+                ans_1 = player_input == 'yes'
+                if ans_1 == False:
+                    if _sum1 > _sum2:
+                        print('Player 1 win with total:', _sum1)
+                        game_over = 1
+                    elif _sum2 > _sum1:
+                        print('Player 2 win with total:', _sum2)
+                        game_over = 1
+                    else:
+                        print('Same total for two players. draw')
+                        game_over = 1
+                else:
+                    break
+        else:
+            break
+
+    if ans_1 == True or ans == True:
+        # take a card
+        suit = random.choice(["❤️", "♦️", "♣️", "♠️"])
+        card = random.choice([2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'])
+        print(suit, card)
+
+        card = (10 if card == "J" or card == "Q" or card == "K" else card)
+        card = (1 if card == "A" else card)
+
+        if x % 2 != 0: # player 1
+            _sum1 += card
+            i += 1
+
+            # win / lose
+            if _sum1 == 21:
+                print("Player's 1 sum is 21 ! Player 1 win!")
+                break
+            if _sum1 > 21:
+                print("Player's 1 total is",_sum1, ". Player 2 win!")
+                break
+
+            # print sum
+            if i > 2 or x > 2:
+                print("Player's 1 sum: ", _sum1)
+
+        else: # player 2
+            _sum2 += card
+            i += 1
+            if _sum2 == 21:
+                print('Players 2 sum is 21 ! Player 1 win!')
+                break
+            if _sum2 > 21:
+                print('Players 2 total is', _sum2, 'Player 2 disqualified. player 1 win!')
+                break
+            if i > 2 or x > 2:
+                print("Player's 2 sum: ", _sum2)
+    else:
+        break
+
+
+
+
+#STOP
--- main.py
+++ main.py
@@ -1,6 +1,84 @@
 #START
 
+# להוסיף מספר תורים שווה לשני שחקנים
+
+
 import random
+
+ans = True
+ans_1 = True
+i = 1
+x = 1
+_sum1 = 0
+_sum2 = 0
+
+while True:
+    # take a card
+    suit = random.choice(["❤️", "♦️", "♣️", "♠️"])
+    card = random.choice([2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'])
+    print(suit, card)
+
+    card = (10 if card == "J" or card == "Q" or card == "K" else card)
+    card = (1 if card == "A" else card)
+
+    if x % 2 != 0: # player 1
+        _sum1 += card # Calculating amounts
+
+        # win / lose
+        if _sum1 == 21:
+            print("Player's 1 sum is 21 ! Player 1 win!")
+            break  # no 'break' fair game
+        if _sum1 > 21:
+            print("Player's 1 total is", _sum1, 'Player 1 disqualified')
+            break
+
+        # print sum
+        if i >= 2 or x > 2:
+            print("Player's 1 sum: ", _sum1)
+
+            # Player selection question
+            player_input:str = input('Want another card? Answer yes/no only').lower()
+            ans = player_input == 'yes'
+
+            # change turn
+        if ans == False:
+            print('change turn')
+            x += 1
+            i = 1
+        else:
+            i += 1
+
+    else:  # player 2
+        ans = True    #reset answer
+        _sum2 += card
+        if _sum2 == 21:
+            print("Player's 2 sum is 21 ! Player 2 win!")
+            break
+        if _sum2 > 21:
+            print("Player's 2 total is",_sum2, "Player 2 disqualified")
+            break
+        if i >= 2 or x > 2:
+            print("Player's 2 sum: ", _sum2)
+            player_input = input('Want another card? Answer yes/no only').lower()
+            ans = player_input == 'yes'
+        if ans == False:
+            print('change turn')
+            x += 1
+            i = 1
+        else:
+            i += 1
+            if
+
+    if _sum1 == _sum2:
+        print('*draw* both players total is equal')
+
+
+
+
+
+
+
+
 
 
 
